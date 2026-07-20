16a267641e8d767428b03968ccc1f6b3fffec297
andybd2703 <yeison.andres2006.yb@gmail.com>
2026-07-20 14:22:10 -0500

    changes in images routes

--- src/app/inicio/Components/inicio-assets.ts
+++ src/app/inicio/Components/inicio-assets.ts
@@ -1,6 +1,6 @@
-export const DESCRIPCION = '/imgs/Mantenimiento-PC.png' as const;
-export const MANTENIMIENTO_FORMATEO = '/imgs/formateo.jpg' as const;
-export const MANTENIMIENTO_FISICO = '/imgs/fisico.jpg' as const;
-export const MANTENIMIENTO_SOFTWARE = '/imgs/software.jpg' as const;
-export const OFFICE = '/imgs/office.webp' as const;
-export const WINDOWS = '/imgs/windows.png' as const;
+export const DESCRIPCION = 'imgs/Mantenimiento-PC.png' as const;
+export const MANTENIMIENTO_FORMATEO = 'imgs/formateo.jpg' as const;
+export const MANTENIMIENTO_FISICO = 'imgs/fisico.jpg' as const;
+export const MANTENIMIENTO_SOFTWARE = 'imgs/software.jpg' as const;
+export const OFFICE = 'imgs/office.webp' as const;
+export const WINDOWS = 'imgs/windows.png' as const;
